d6132bef29092006a46a5c81de7b3f72b4e84de5
Justyna Rudnicka-Staniewska <jete.justyce@gmail.com>
2017-04-20 19:26:50 +0200

    add basic structure

---
+++ js/script.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var dropbtn = document.querySelector(".dropbtn");
+    var dropmenu = document.querySelector(".dropdown-menu");
+    
+    var mobileScreen = window.matchMedia("screen and (max-width: 767px)");
+    
+    function visibility(mobileScreen) {
+        if (mobileScreen.matches) {
+            dropmenu.style.display = "none";
+            dropbtn.style.display = "block";
+        } else {
+            dropmenu.style.display = "flex";
+            dropbtn.style.display = "none";
+        }
+    }
+    
+    visibility(mobileScreen);
+    
+    mobileScreen.addListener(visibility);
+    
+    dropbtn.addEventListener("click", function(){
+        if (dropmenu.style.display == "none") {
+            dropmenu.style.display = "block";
+        } else {
+            dropmenu.style.display = "none";
+        }
+    });
+});
